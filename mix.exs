d513f46fcf56647c64df9582834fb856a97204ac
Franz Bettag <franz@bett.ag>
2021-10-25 23:44:10 +0200

    updates a few dependencies.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule PhoenixTailwindNotifications.MixProject do
   def project do
     [
       app: :phoenix_tailwind_notifications,
-      version: "0.1.6",
+      version: "0.1.7",
       elixir: "~> 1.7",
       source_url: @project_url,
       homepage_url: @project_url,
@@ -34,7 +34,7 @@ defmodule PhoenixTailwindNotifications.MixProject do
 
   defp deps do
     [
-      {:phoenix_live_view, "~> 0.15"},
+      {:phoenix_live_view, "~> 0.16"},
       {:ex_doc, "~> 0.19", only: :dev},
       {:credo, github: "rrrene/credo", only: [:dev, :test]}
     ]
